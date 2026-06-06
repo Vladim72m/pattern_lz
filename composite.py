247dd05c16fae9b4468fa9c3a55f2377ac26d0fd
Vladim72m <vladim72dx@tutamail.com>
2026-06-06 23:49:04 +0300

    Update composite.py

--- composite.py
+++ composite.py
@@ -1,23 +1,17 @@
-# Вариант 4
-from abc import ABC, abstractmethod
-
-class Component(ABC):
-    @abstractmethod
-    def show(self, indent=""): pass
-
-class File(Component):
+class File:
     def __init__(self, name):
         self.name = name
 
     def show(self, indent=""):
         print(f"{indent}- {self.name}")
 
-class Folder(Component):
+
+class Folder:
     def __init__(self, name):
         self.name = name
         self.items = []
 
-    def add(self, item: Component):
+    def add(self, item):
         self.items.append(item)
 
     def show(self, indent=""):
