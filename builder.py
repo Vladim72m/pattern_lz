b206c96edbb781c0f6e2b5a936e3abe9e0bbbf9a
Vladim72m <vladim72dx@tutamail.com>
2026-06-06 23:48:10 +0300

    Update builder.py

--- builder.py
+++ builder.py
@@ -1,18 +1,64 @@
-# вариант 4 
-from abc import ABC, abstractmethod
-
 class Backpack:
     def __init__(self):
-        self.contents = []  
-class IBuilder(ABC): 
-    @abstractmethod
-    def add_parts(self): pass
+        self.contents = []
+        self.food = None
+        self.water = None
 
-class MathDayBuilder(IBuilder):
+    def __str__(self):
+        result = "Рюкзак:\n"
+        for item in self.contents:
+            result += f"- {item}\n"
+        result += f"Еда: {self.food if self.food else 'нет'}\n"
+        result += f"Вода: {self.water if self.water else 'нет'}\n"
+        return result
+
+
+class BackpackBuilder:
+    def add_books(self):
+        pass
+    def add_tools(self):
+        pass
+    def add_food(self):
+        pass
+    def add_water(self):
+        pass
+    def get_result(self) -> Backpack:
+        pass
+
+
+class MathDayBuilder(BackpackBuilder):
     def __init__(self):
         self.backpack = Backpack()
-    
-    def add_parts(self):
-        self.backpack.contents.extend(["Учебник геометрии", "Тетрадь в клетку", "Циркуль", "Калькулятор"])
+
+    def add_books(self):
+        self.backpack.contents.append("Учебник геометрии")
+        self.backpack.contents.append("Тетрадь в клетку")
+
+    def add_tools(self):
+        self.backpack.contents.append("Циркуль")
+        self.backpack.contents.append("Линейка")
+        self.backpack.contents.append("Калькулятор")
+
+    def add_food(self):
+        self.backpack.food = "Бутерброд"
+
+    def add_water(self):
+        self.backpack.water = "Бутылка воды"
+
+    def get_result(self) -> Backpack:
         return self.backpack
 
+class Director:
+    def __init__(self, builder: BackpackBuilder = None):
+        self.builder = builder
+
+    def build_minimal_pack(self):
+        self.builder.add_books()
+        self.builder.add_tools()
+
+    def build_full_pack(self):
+        self.builder.add_books()
+        self.builder.add_tools()
+        self.builder.add_food()
+        self.builder.add_water()
+          
