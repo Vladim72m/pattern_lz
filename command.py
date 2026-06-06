6eb4c126517e6c9f121141b5f8ce4d2f19dee7ba
Vladim72m <vladim72dx@tutamail.com>
2026-06-06 23:48:40 +0300

    Update command.py

--- command.py
+++ command.py
@@ -1,22 +1,67 @@
-# Вариант 4
-from abc import ABC, abstractmethod
+# Вариант 4 
 
 class Light:
-    def turn_on(self): print("Свет горит")
-    def turn_off(self): print("Свет погас")
+    def __init__(self):
+        self.is_broken = False
 
-class ICommand(ABC):  
-    @abstractmethod
-    def execute(self): pass
+    def on(self):
+        if self.is_broken:
+            print("Свет выключен.")
+        else:
+            print("Свет включен.")
 
-class OnCommand(ICommand):
-    def __init__(self, light): self.light = light
-    def execute(self): self.light.turn_on()
+    def break_lamp(self):
+        self.is_broken = True
 
-class OffCommand(ICommand):
-    def __init__(self, light): self.light = light
-    def execute(self): self.light.turn_off()
 
-class Remote:
-    def __init__(self): self.cmd = None
-    def press(self): self.cmd.execute()
+class Command:
+    def __init__(self, receiver, action):
+        self.receiver = receiver
+        self.action = action
+
+    def execute(self):
+        getattr(self.receiver, self.action)()
+
+
+class Button:
+    def __init__(self, name, command):
+        self.name = name
+        self.command = command
+
+    def click(self):
+        print(f"[Нажата кнопка '{self.name}']")
+        self.command.execute()
+        # Вариант 4 
+
+class Light:
+    def __init__(self):
+        self.is_broken = False
+
+    def on(self):
+        if self.is_broken:
+            print("Свет выключен.")
+        else:
+            print("Свет включен.")
+
+    def break_lamp(self):
+        self.is_broken = True
+
+
+class Command:
+    def __init__(self, receiver, action):
+        self.receiver = receiver
+        self.action = action
+
+    def execute(self):
+        getattr(self.receiver, self.action)()
+
+
+class Button:
+    def __init__(self, name, command):
+        self.name = name
+        self.command = command
+
+    def click(self):
+        print(f"[Нажата кнопка '{self.name}']")
+        self.command.execute()
+        
